feeda09e43c545de1427369f88dfccde3551b8d3
Jan Premzl <jan.premzl16@gmail.com>
2022-10-22 16:47:31 +0200

    Progress

--- nn/nn.py
+++ nn/nn.py
@@ -1,27 +1,27 @@
 import torch
-from nn.firstbranch import FirstBranch
 from nn.secondbranch import SecondBranch
 
 
 class NeuralNetwork(torch.nn.Module):
     def __init__(self) -> None:
         super().__init__()
-        self.first_branch = FirstBranch()
+        self.filter_layer = torch.nn.Conv2d(
+            1, 8, kernel_size=(11, 11), padding="same", bias=False)
         self.second_branch = SecondBranch()
 
     def forward(self, x, r, g, b):
-        r = self.first_branch(r)
-        g = self.first_branch(g)
-        b = self.first_branch(b)
+        r = self.filter_layer(r)
+        g = self.filter_layer(g)
+        b = self.filter_layer(b)
 
         x = self.second_branch(x)
 
-        r = x * r
-        g = x * g
-        b = x * b
+        r = torch.multiply(x, r)
+        g = torch.multiply(x, g)
+        b = torch.multiply(x, b)
 
-        r = torch.sum(r, dim=1)
-        g = torch.sum(g, dim=1)
-        b = torch.sum(b, dim=1)
+        r = torch.sum(r, dim=1, keepdim=True)
+        g = torch.sum(g, dim=1, keepdim=True)
+        b = torch.sum(b, dim=1, keepdim=True)
 
-        return torch.stack((r, g, b), dim=1)
+        return torch.cat((r, g, b), dim=1)
--- nn/resnet.py
+++ nn/resnet.py
@@ -6,7 +6,7 @@ class Resnet(torch.nn.Module):
         super().__init__()
         self.first_conv2d = torch.nn.Conv2d(
             3, 32, kernel_size=(3, 3), padding="same")
-        #self.batchnorm2d = torch.nn.BatchNorm2d()
+        self.batchnorm2d = torch.nn.BatchNorm2d(32)
         self.dropout2d = torch.nn.Dropout2d(p=0.5)
         self.relu = torch.nn.ReLU()
         self.second_conv2d = torch.nn.Conv2d(
@@ -22,17 +22,17 @@ class Resnet(torch.nn.Module):
         else:
             x = self.second_conv2d(x)
 
-        #x = self.batchnorm2d(x)
+        x = self.batchnorm2d(x)
         x = self.dropout2d(x)
         x = self.relu(x)
         x = self.second_conv2d(x)
-        #x = self.batchnorm2d(x)
+        x = self.batchnorm2d(x)
         x = self.dropout2d(x)
 
         if first:
             y = self.third_conv2d(y)
 
-        x = x + y
+        x = torch.add(x, y)
 
         x = self.relu(x)
 
